6d74db5058970cbfb76bd9463d2d3ac164e5593e
hunx <junaidhunain6@gmail.com>
2023-11-12 23:14:12 +0500

    integrate buymeacoffee

--- src/creditbar.js
+++ src/creditbar.js
@@ -23,7 +23,8 @@ export default function CreditBar(props){
                     Coded with 🤍  by <a href="https://github.com/hunxjunedo">hunxjunedo</a>
                     <br/>
                     Shot by <a href={credits.link}>{credits.name}</a> @ <a href="https://www.pexels.com/"> Pexels</a>
-          </>
+                    <br/>
+                    <a  href="https://www.buymeacoffee.com/hunxjunedo"><img style={{marginTop: 10}} src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=☕&slug=hunxjunedo&button_colour=ffa500&font_colour=000000&font_family=Inter&outline_colour=000000&coffee_colour=FFDD00" /></a>          </>
                  :
                 '🤍'
         
